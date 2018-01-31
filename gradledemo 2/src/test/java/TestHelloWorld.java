3de1f0b54c60034380388315388e6c839d66b532
Michelle Liu <michelleliu343@gmail.com>
2018-01-30 21:12:08 -0500

    gradle file builds but shows error
    builds successfully but cannot find or load AntLabMain

---
+++ gradledemo 2/src/test/java/TestHelloWorld.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * test class.
+ */
+public final class TestHelloWorld {
+    /**
+     * test method.
+     */
+    @Test
+    public void testMain() {
+       System.out.println("Testing the Main Module");
+       assertTrue(true);
+    }
+}
