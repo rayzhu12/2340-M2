5aca1c6d53f6dce9510cf845ee10bbf71d39a45f
Michelle Liu <michelleliu343@gmail.com>
2018-01-30 22:02:38 -0500

    whoops

---
+++ M2/gradledemo2/src/main/java/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * class prints out hello.
+*/
+public final class HelloWorld {
+   /**
+    * main method.
+    * @param args not used
+    */
+   public static void main(final String[] args) {
+       System.out.println("Hello World");
+       //int x = ;
+   }
+
+   /**
+    * make constructor private.
+    */
+   private HelloWorld() { }
+}
+
